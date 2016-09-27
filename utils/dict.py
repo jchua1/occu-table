ad6b1da09c42ea5f43ae9c0bb5b187a7f7fd7ab4
jchua1 <jchua@stuy.edu>
2016-09-27 12:20:58 -0400

    Added utils and data directories

---
+++ utils/dict.py
@@ -0,0 +1,32 @@
+import csv
+from random import choice, uniform
+
+occs = {}
+
+def readToDict():
+    import csv
+    with open("data/occupations.csv") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            job = row["Job Class"]
+            percent = float(row["Percentage"])
+            link = row["Link"]
+            if (job != "Total"):
+                occs[job] = [percent, link]
+                
+def pickWeighted():
+    readToDict()
+    rand = uniform(0, 99.8)
+    x = 0.0
+    for job, value in occs.iteritems():
+        x += value[0]
+        if rand <= x:
+            return job
+    return job
+
+def getJob():
+    readToDict()
+    return pickWeighted()
+
+def getLink(job):
+    return occs[job][1]
